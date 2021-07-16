6406bf6eeb3f8456b9371108b8475954776067ef
avinashbn2 <avinashbn@protonmail.com>
2021-07-16 19:40:03 +0530

    init repo

---
+++ _next/static/xJUWsaIqg8yba2dB8hElC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F95bb7b8da3b6c95f4bb6.css","static\u002Fchunks\u002Fpages\u002Findex-d204969661f7c0441e12.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4798f608fd72b7a2d4bc.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
